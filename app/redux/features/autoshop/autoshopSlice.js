868178418749fd0702bdd8615c1651302cde1613
GevS <gevs.sargsyan96@gmail.com>
2024-04-10 17:09:20 +0400

    add autoshopSlice

---
+++ app/redux/features/autoshop/autoshopSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = []
+
+export const autoshopSlice = createSlice({
+    name: "autoshop",
+    initialState,
+    reducers:{
+
+    }
+})
+
+export const {} = autoshopSlice.actions
+export default autoshopSlice.reducer
--- app/redux/store.js
+++ app/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import autoshopSlice from './features/autoshop/autoshopSlice'
 
 export const store = configureStore({
     reducer: {
-        
+        autoshop: autoshopSlice
     }
 })
