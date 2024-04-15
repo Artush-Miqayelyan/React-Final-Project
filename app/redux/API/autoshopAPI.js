7f9055fbd5b9dfd478a0c0fdf4bb9d81369eec00
Artush-Miqayelyan <miqaelyan.artush@mail.ru>
2024-04-15 13:43:22 +0400

    Url change to 4000localhost

--- app/redux/API/autoshopAPI.js
+++ app/redux/API/autoshopAPI.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
-const Url = 'http://localhost:3000/cars'
+const Url = 'http://localhost:4000/cars'
 
 export const getCars = createAsyncThunk(
     'autoshop/getCars',
