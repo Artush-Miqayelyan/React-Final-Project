8a3a1c7d658fa86e358262aeea302886818fb624
Gohar Papoyan <gogarenia7@gmail.com>
2024-04-25 23:22:37 +0400

    some changes

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,23 +4,24 @@ function Hello() {
     return(
         <div className = {styles.about}>
         <div className = {styles.title}> 
-            <h3> Նախագծի մասին </h3>
+            <h3> About the project </h3>
         </div>
        <div className = {styles.aboutInfo}>
        <p  className = {styles.text}> 
-       <strong> Carshop </strong> կայքն օգնում է գնել եւ վաճառել ավտոմեքենաներ եւ այլ տրանսպորտային միջոցներ։ </p>
+       <strong> Carshop </strong> website helps to buy and sell cars and other vehicles. </p>
 
-<p className = {styles.text}><strong> Carshop </strong> կայքը ստեղծվել է վերջերս: Կարծում ենք այն անհրաժեշտ եւ օգտակար գործիք է դարձել ինչպես ավտոսիրողների, այնպես էլ շահագրգիռ կազմակերպությունների համար:</p>
+<p className = {styles.text}><strong> Carshop </strong> website was created recently. We think it has become a necessary and useful tool for both car enthusiasts and interested organizations.</p>
 
-<p className = {styles.text}>Գործարկման օրվանից սկսած մենք մշտապես կատարելագործել ենք կայքը եւ առաջարկել նոր ծառայություններ, ինչը թույլ է տվել մեզ չզիջել դիրքերը եւ պահպանել առաջատարի դերը:</p>
+<p className = {styles.text}>Since the day of launch, we have constantly improved the site and offered new services, which allowed us not to give up our positions and maintain the leading role.:</p>
 
-<p className = {styles.text}> Ամենայն հավանականությամբ այն ամենաայցելվող ավտոմոբիլային կայքերից է հայկական ինտերնետում  </p>
+<p className = {styles.text}> It is probably one of the most visited automotive websites on the Armenian Internet. </p>
 
-<p className = {styles.text}>Այսօր <strong> Carshop </strong>-ն ունի վաճառվող ավտոտրանսպորտային միջոցների ամենաընդարձակ բազան եւ ամենաայցելվող կայքն է ավտոմեքենաների առք ու վաճառքի ոլորտում:</p>
+<p className = {styles.text}>Today, <strong> Carshop</strong> has the largest database of vehicles for sale and is the most visited website in the field of buying and selling cars.
+</p>
 
-<p className = {styles.text}> Մենք ուրախ ենք ներկայացնել ձեզ <strong> Carshop </strong>-ի ամենավերջին տեխնոլոգիական լուծումները, որոնք շատ ավելի հարմարավետ եւ հեշտ են դարձնում դրանից օգտվելը` սկսած գրանցումից մինչեւ հայտարարության տեղադրումը:</p>
+<p className = {styles.text}> We are pleased to introduce you to the latest technological solutions of <strong> Carshop</strong>, which make it much more convenient and easy to use it, from registration to placing an ad.</p>
 
-<p  className = {styles.text}> Հարգանքով` <b> Carshop:</b></p>
+<p  className = {styles.text}> Sincerely, <b> Carshop:</b></p>
         
  
        </div>
@@ -29,5 +30,20 @@ function Hello() {
     </div>
     )
 }
+/* 
+   About the project
+Carshop website helps to buy and sell cars and other vehicles.
 
+The Carshop website was created recently. We think it has become a necessary and useful tool for both car enthusiasts and interested organizations.
+
+Since the day of launch, we have constantly improved the site and offered new services, which allowed us not to give up our positions and maintain the leading role.
+
+It is probably one of the most visited automotive websites on the Armenian Internet
+
+Today, Carshop has the largest database of vehicles for sale and is the most visited website in the field of buying and selling cars.
+
+We are pleased to introduce you to the latest technological solutions of Carshop, which make it much more convenient and easy to use it, from registration to placing an ad.
+
+Sincerely, Carshop.
+*/
 export default Hello
