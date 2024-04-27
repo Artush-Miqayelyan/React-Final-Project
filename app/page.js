52a8b86b981cd03500dbc37ada3698d7a4850d28
Artush-Miqayelyan <miqaelyan.artush@mail.ru>
2024-04-27 09:49:37 +0400

    setting users for store

--- app/page.js
+++ app/page.js
@@ -5,7 +5,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import {
   selectCars
 } from './redux/features/autoshop/autoshopSlice'
+import {
+  selectUsers
+} from './redux/features/users/usersSlice'
 import { getCars } from "./redux/API/autoshopAPI"
+import { getUsers } from "./redux/API/usersAPI"
 import Main from "./components/main/main"
 import FilterBar from './components/FilterBar/filterBar'
 import "./global.css"
@@ -22,13 +26,23 @@ export default function Home() {
   const cars = useSelector(selectCars)
   const filteredCarsData = useSelector(selectFilteredCars)
 
-  useEffect(() => {
-    console.log("FILTERED :::: " , filteredCarsData)
-  } , [filteredCarsData])
+  const users = useSelector(selectUsers)
+
+  // useEffect(() => {
+  //   console.log("FILTERED :::: ", filteredCarsData)
+  // }, [filteredCarsData])
 
   useEffect(() => {
     dispatch(getCars())
   }, [])
+
+  useEffect(() => {
+    dispatch(getUsers())
+  }, [])
+
+  // useEffect(() => {
+  //   console.log("USERS ::: " , users)
+  // } , [users])
 
   return <div className="main" >
     <FilterBar />
